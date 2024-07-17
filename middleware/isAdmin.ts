29a376b498a2b4186dfbc5661c6e33ed53997405
Digerido <77206237+Digerido@users.noreply.github.com>
2024-07-17 19:27:04 +0700

    fix

--- middleware/isAdmin.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const userStore = useUserStore();
-  if (
-    userStore.user?.active_role !== "moderator" &&
-    userStore.user?.active_role !== "admin"
-  ) {
-    return navigateTo("/login?from=" + from.path);
-  }
-});
